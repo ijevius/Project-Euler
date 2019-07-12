15ef60bfaa86778805884640c6247bb468044689
Ruslan Jevik <47752953+ijevius@users.noreply.github.com>
2019-07-12 19:52:49 +0300

    Update Ex4.py
    10x fewer iteration and 6x faster vs http://pythoneuler.blogspot.com/2016/09/4.html

--- Ex4.py
+++ Ex4.py
@@ -1,10 +1,29 @@
+import sys
+import time
+
 b = 100001
+big_m = 0
+big_i = 0
+s = 0
+
+z = time.time()
 
 for i in range(999999, 100000, -1):
     if str(i)[:3] == str(i)[3:][::-1]:
         for m in range(100, 1000):
+            if i < big_i and m < big_m:
+                print(b);
+                x = time.time();
+                print("%.20f" % float(x-z));
+                print(f's = {s}');
+                sys.exit();
             n = i/m
+            s += 1
             if n.is_integer():
                 if len(str(int(n))) == 3 and i > b:
                     b = i
+                    big_m = m; big_i = i;
 print(b)
+x = time.time()
+print("%.20f" % x-z)
+print(f's = {s}')
